38dd7016afe4c25bdc0a17ffdb4947d63ca2739f
Doyeon Kim <archdo0909@gmail.com>
2020-10-14 18:50:56 +0900

    Add dataset loader main.py

---
+++ src/main.py
@@ -0,0 +1,16 @@
+from .ai_champ import champ_Dataset
+
+def load_dataset(dataset_name, data_path, normal_class, random_state=None):
+
+    implemented_datasets = ('ai_champ')
+
+    assert dataset_name in implemented_datasets
+
+    dataset = None
+
+    if dataset_name == 'ai_champ':
+        dataset = champ_Dataset(root=data_path,
+                                normal_class=normal_class,
+                                )
+    
+    return dataset
