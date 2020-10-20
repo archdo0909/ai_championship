d0f8d9a730644488af833650a95b72fdc0808035
Doyeon Kim <archdo0909@gmail.com>
2020-10-20 14:56:18 +0900

    데이터 로더의 getitem 수정

--- src/base/lg_dataset.py
+++ src/base/lg_dataset.py
@@ -22,20 +22,54 @@ class LGDataset(Dataset):
         self.dataset_name = dataset_name
         self.train = train
         self.folder_path = self.root / self.dataset_name
-
-        # Read all data
-        X, y, stage, degc = read_data()
+        self.label_path = self.folder_path / "label"
         
+        # catch label data
+        fld = self.label_path.glob("**/*")
+        files = fld[0]
+
+        """load label data
+
+        input: "label_data.txt"
+        column name:
+            [label] [measure time] [stage] [temperature]
+            
+        X contains [measure time] [stage] [temperature] [file_num]
+        y contains [label]
+
+        """
+        data = []
+        with open(file, "r") as f:
+            while 1:
+                line = f.readline()
+                if not line:
+                    break
+                data.append(line.strip().split("\t"))
+        
+        data = np.array(data)
+
+        X = data[:, 1:]
+        y = data[:, 0]
+
+
+        """
+        split train, test and validation set
+        train : 60%, test: 20%, validation: 20% 
+
+        """
         X_train, X_test, y_train, y_test = train_test_split(X, y,
                                                             test_size=0.4,
                                                             random_state=random_state)
-
+        X_test, X_val, y_test, y_val = train_test_split(X_test, y_test,
+                                                        test_size=0.5,
+                                                        random_state=random_state)
+        
         if self.train:
-            self.data = torch.tensor(X_train.to_numpy())
+            self.data = X_train
             self.targets = torch.tensor(y_train.to_numpy())
         else:
-            self.data = torch.tensor(X_test.to_numpy())
-            self.targets = torch.tensor(y_test.to_numpy())
+            self.data = X_train
+            self.targets = torch.tensor(y_train.to_numpy())
 
     def __getitem__(self, index):
         """
@@ -46,7 +80,28 @@ class LGDataset(Dataset):
         Returns:
             tuple: (sample, target, index)
         """
+        base_file_name = "_FLD165NBMA_vib_spectrum_modi_train_"
+
         sample, target = self.data[index], int(self.targets[index])
+        measuretime = self.data[index][0]
+        file_num = self.data[index][-1]
+        
+        # search Hz data
+        target_fname = str(measuretime[:8]) \
+                       + base_file_name \
+                       + str(file_num) \
+                       + ".txt"
+
+        f = open(self.folder_path/target_fname, 'r')
+        while 1:
+            line = f.readline()
+            if not line:
+                break
+            if measuretime in line: 
+                sample = line.strip().split('\t')[5:]
+        f.close()
+
+        target = int(self.tagets[index])
 
         return sample, target, index
 
