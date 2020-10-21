34689bf9a35ef504d4e659d604a5a169e6cfc42f
root <Doyeon Kim>
2020-10-21 06:11:09 +0000

    데이터 로더의 인풋 수정

--- src/datasets/main.py
+++ src/datasets/main.py
@@ -1,17 +1,24 @@
 from .lg import LGADDataset
 from .mnist import MNIST_Dataset
 
-def load_dataset(dataset_name, data_path, random_state=None):
+def load_dataset(dataset_name, data_path, normal_class, known_outlier_class, n_known_outlier_classes: int = 0,
+                 ratio_known_normal: float = 0.0, ratio_known_outlier: float = 0.0, ratio_pollution: float = 0.0,
+                 random_state=None):
+    """Loads the dataset."""
 
     implemented_datasets = ('lg_train', 'mnist')
 
     assert dataset_name in implemented_datasets
 
     dataset = None
-
-    if dataset_name == 'lg_train':
-        dataset = LGADDataset(root=data_path, 
-                              dataset_name=dataset_name)
+    if dataset_name in ('lg'):
+        dataset = LGADDataset(root=data_path,
+                                dataset_name=dataset_name,
+                                n_known_outlier_classes=n_known_outlier_classes,
+                                ratio_known_normal=ratio_known_normal,
+                                ratio_known_outlier=ratio_known_outlier,
+                                ratio_pollution=ratio_pollution,
+                                random_state=random_state)
     
     if dataset_name == 'mnist':
         dataset = MNIST_Dataset(root=data_path,
