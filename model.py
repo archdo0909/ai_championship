80689d62a26138fdec91034052dff338b27969fc
Jinsung Ha <jsung5381@naver.com>
2020-11-06 00:29:47 +0900

    Include UNet as a candidate model, output shape needs an adjustment

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 from torchvision.models import resnet34
 
 
-class Peter_CNN(nn.Module):
+class PeterCNN(nn.Module):
     def __init__(self):
         super(Peter_CNN, self).__init__()
 
@@ -29,15 +29,67 @@ class Peter_CNN(nn.Module):
         return num_features
 
 
-def build_network(net_name):
+class UNet(nn.Module):
+    def double_conv(in_channels, out_channels):
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(out_channels),
+            nn.Dropout(0.05),
+            nn.Conv2d(out_channels, out_channels, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(out_channels),
+            nn.Dropout(0.05),
+        )   
 
-    implemented_networks = ('Peter_CNN', )
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+                
+        self.dconv_down1 = self.double_conv(in_channels, 64)
+        self.dconv_down2 = self.double_conv(64, 128)
+        self.dconv_down3 = self.double_conv(128, 256)
+        self.dconv_down4 = self.double_conv(256, 512)        
 
-    assert net_name in implemented_networks
+        self.maxpool = nn.MaxPool2d(2)
+        
+        self.dconv_up3 = self.double_conv(256 + 512, 256)
+        self.dconv_up2 = self.double_conv(128 + 256, 128)
+        self.dconv_up1 = self.double_conv(128 + 64, 64)
+        
+        self.conv_last = nn.Conv2d(64, out_channels, 1)
+        
+    def forward(self, x):
+        conv1 = self.dconv_down1(x)
+        x = self.maxpool(conv1)
 
-    net = None
+        conv2 = self.dconv_down2(x)
+        x = self.maxpool(conv2)
+        
+        conv3 = self.dconv_down3(x)
+        x = self.maxpool(conv3)   
+        
+        x = self.dconv_down4(x)
+        
+        x = F.interpolate(x, scale_factor=2, mode='bilinear', align_corners=True)
+        x = torch.cat([x, conv3], dim=1)
+        
+        x = self.dconv_up3(x)
+        x = F.interpolate(x, scale_factor=2, mode='bilinear', align_corners=True)
+        x = torch.cat([x, conv2], dim=1)       
 
-    if net_name == 'Peter_CNN':
-        net = Peter_CNN()
-    
-    return net
+        x = self.dconv_up2(x)
+        x = F.interpolate(x, scale_factor=2, mode='bilinear', align_corners=True)
+        x = torch.cat([x, conv1], dim=1)   
+        
+        x = self.dconv_up1(x)
+        return self.conv_last(x)
+
+def build_network(network_name):
+    implemented_networks = ('PeterCNN', 'UNet',)
+    assert network_name in implemented_networks, 'invaliad network name'
+    network = {
+        'vanilla_cnn': PeterCNN(),
+        'UNet': UNet(),
+    }.get(network_name)
+    return network
+
