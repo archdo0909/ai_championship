c1146e49828194164fcf1d61cd6367ed3feafff5
Doyeon <archdo0909@gmail.com>
2020-11-07 09:01:37 +0000

    Refactor sampler to load one file only

--- sample.py
+++ sample.py
@@ -1,4 +1,3 @@
-import glob
 import numpy as np
 
 from imblearn.over_sampling import SMOTE, SMOTENC
@@ -8,23 +7,22 @@ class Sampler:
     """A sampler performs over and under samling on given dataset"""
 
     def __init__(self, src_path, dst_path):
-        self.files_to_load = glob.glob(src_path)
-        self.files_to_save = dst_path
+        self.file_to_load = src_path
+        self.file_to_save = dst_path
         
     def load_data(self, nb_data_to_load):
         # Load structured data given by LG Science Park 
-        for idx, fname in enumerate(self.files):
-            X = []
-            y = []
-            with open(fname, 'r') as f:
-                lines = f.readlines()
-                for line in lines:
-                    if len(X) == nb_data_to_load:
-                        break
-                    curr_data = line.strip().split('\t')
-                    curr_data[2] = stage_value = int(curr_data[2][1])
-                    X.append(curr_data[1:])
-                    y.append(curr_data[0])
+        X = []
+        y = []
+        with open(self.file_to_load, 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                if len(X) == nb_data_to_load:
+                    break
+                curr_data = line.strip().split('\t')
+                curr_data[2] = stage_value = int(curr_data[2][1])
+                X.append(curr_data[1:])
+                y.append(curr_data[0])
         return X, y
 
     def oversample(self, nb_data_to_load):
@@ -39,7 +37,7 @@ class Sampler:
         X_resampled, y_resampled = oversampler.fit_sample(X_np, y_np)
         
         # Save oversampled data to a text file
-        with open(self.files_to_save, 'a') as f:
+        with open(self.file_to_save, 'a') as f:
             for data, label in zip(X_resampled, y_resampled):
                 line = str(label) + '\t' + '\t'.join(map(str, data)) + '\n'
                 f.write(line)
@@ -48,10 +46,6 @@ class Sampler:
         raise NotImplementedError
 
 
-def main():
-    au = Sampler()
-    au.augment(1000)
-
-
 if __name__ == "__main__":
-    main()
+    sampler = Sampler('', '')
+    sampler.oversmaple(100)
