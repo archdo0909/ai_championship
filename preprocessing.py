8b1c62dc2157a391d62eee0eac0a3dc33809f153
wsy8029 <wsy8029@gmail.com>
2020-11-07 12:49:55 +0000

    Add preprocessing step

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,19 @@
 import numpy as np
 
 
-def preprocess(np_data):
-    np_data = np_data.reshape(100, -1)
-    return np_data
+def preprocess(curr_data):
+    freqs = curr_data[3:]
+    freqs_image = freqs.reshape(100, -1)
+
+    stage = curr_data[1]
+    temperature = curr_data[2]
+
+    stage_channel = np.full(freqs_image.shape, stage, dtype = np.int8)
+    temperature_channel = np.full(freqs_image.shape, temperature, dtype = np.float)
+
+    stage_channel = stage_channel[np.newaxis, :, :]
+    temperature_channel = temperature_channel[np.newaxis, :, :]
+
+    freqs_image = np.concatenate((freqs_image, stage_channel), axis=0)
+    freqs_image = np.concatenate((freqs_image, temperature_channel), axis=0)
+    return freqs_image
